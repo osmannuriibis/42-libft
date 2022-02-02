26984bf277cec7ada262bdf93384b651a0bfad6e
Osman nuri Ibis <oibis@k2m18s05.42kocaeli.com.tr>
2022-02-02 21:16:59 +0300

    campus

---
+++ Libft/ft_strlcpy.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oibis <oibis@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/02 20:02:29 by oibis             #+#    #+#             */
+/*   Updated: 2022/02/02 21:14:54 by oibis            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+size_t  ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+    const char *s;
+    
+    s = src;
+	i = 0;
+	while (*s && i < size - 1 && size != 0)
+	{
+		dest[i++] = *s++;
+	}
+    
+    if (size == 0)
+    {
+        return (ft_strlen(src));
+    }
+    else
+	    dest[i] = '\0';
+        
+	return (s - src);
+}
---
+++ Libft/libft.h
@@ -0,0 +1,4 @@
+
+/* DO NOT REMOVE THIS COMMENT: fixincludes needs to see:
+ * _GCC_SIZE_T */
+#include <unistd.h>
